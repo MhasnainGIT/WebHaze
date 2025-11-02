facd894a0624810d281f56e1f858e9e9942278a6
MhasnainGIT <mohdhasnain1544@gmail.com>
2025-11-02 11:26:48 +0530

    backend changes in the index.js

--- server/index.js
+++ server/index.js
@@ -123,7 +123,9 @@ const corsOptions = {
     }
   },
   credentials: true,
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 };
 
 app.use(cors(corsOptions));
